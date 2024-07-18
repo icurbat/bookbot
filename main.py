0e42c2533869ce809cff9eba2b9f5482b150542e
icurbat <ioan.curbat@gmail.com>
2024-07-18 17:40:30 +0300

    Add generate_report

--- main.py
+++ main.py
@@ -3,17 +3,37 @@ def main():
     text = get_book_text(path)
     number_of_words = count_words(text)
     character_dictionary = count_characters(text)
-    print(sort_on(character_dictionary))
+    print(generate_report(character_dictionary))
 
 def get_book_text(path):
+    """
+    Retrieve the text from a book.
+
+    :param path: str - the file path to the text file containing the book.
+    :return: str - the complete text extracted from the specified file.
+    """
+
     with open(path) as f:
         return f.read()
     
 def count_words(book):
+    """
+    Count the number of words found in a book.
+
+    :param book: str - containing the text from the book.
+    :return: int - count of words found in the text. 
+    """
+
     words = book.split()
     words_count = len(words)
     
 def count_characters(book):
+    """
+    Count the number of characters found in a book.
+
+    :param book: str - containing the text from the book.
+    :return: dictionary - of characters and count of characters found in the text.
+    """
     character_dictionary = {}
     for letter in book:
         lowered_letter = letter.lower()
@@ -23,6 +43,15 @@ def count_characters(book):
             character_dictionary[lowered_letter] = 1
     return character_dictionary    
 
-def generate_report(book):
+def generate_report(dictionary):
+    """
+    
+
+    :param book: str - containing the text from the book.
+    :return: int - count of characters found in the text.
+    """
+    dictionary_list = [{key: value} for key, value in dictionary.items()]
+    dictionary_list.sort(reverse = True, key = sort_on)
+    return dictionary_list
     
 main()
